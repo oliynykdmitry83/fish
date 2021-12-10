cbb1aefcf34aa28264a1fe320d15320447eb412b
Dmytro <oliynykdmitry83@gmail.com>
2021-12-10 14:57:13 +0200

    finished the third part

--- js/main.js
+++ js/main.js
@@ -4,7 +4,31 @@ $(function(){
       dots: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
-      vertical: true,
+      fade: true,
    });
+
+
+   $('.product__name').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      // arrows: false,
+      // fade: true,
+      focusOnSelect: true,
+      asNavFor: '.product__content',
+      centerMode: true,
+      vertical: true,
+      prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    });
+    $('.product__content').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.product__name',
+      // dots: true,
+      // centerMode: true,
+      // focusOnSelect: true,
+      fade: true,
+      arrows: false,
+    });
 
 });

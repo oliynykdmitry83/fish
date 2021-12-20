8b61fe2f6c55c14ab5a737ce4a95f77655c30e79
Dmytro <oliynykdmitry83@gmail.com>
2021-12-20 16:31:38 +0200

    adaptive contacts&footer

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ $(function(){
       prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
       fade: true,
+      responsive: [
+         {
+            breakpoint: 361,
+            settings: {
+               dots: false,
+               arrows: false,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            }
+         }
+      ]
    });
 
 
@@ -19,8 +30,28 @@ $(function(){
       vertical: true,
       prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
       nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
-    });
-    $('.product__content').slick({
+      responsive: [
+         {
+            breakpoint: 891,
+            settings: {
+               vertical: false,
+               slidesToShow: 3,
+               arrows: false,
+               centerMode: true,
+               dots: true
+            },
+            breakpoint: 461,
+            settings: {
+               vertical: false,
+               slidesToShow: 1,
+               arrows: false,
+               dots: true,
+            }
+         }
+      ]
+   });
+   
+   $('.product__content').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       asNavFor: '.product__name',
@@ -29,6 +60,10 @@ $(function(){
       // focusOnSelect: true,
       fade: true,
       arrows: false,
-    });
+   });
 
-});
+   $('.menu__btn').on('click', function(){
+      $('.menu__list').toggleClass('menu__list--active');
+   });
+
+});  
